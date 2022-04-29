88b14cd5cddc4c929876acaf27a521bfe9751a6f
Jacob <jacob.peat01@gmail.com>
2022-04-29 12:07:14 +0100

    Add basic JSX structure and render to page. Find out why images not loading

--- src/Main.js
+++ src/Main.js
@@ -3,8 +3,13 @@ import React from "react"
 
 export default function Main(props){
     return (
-            <div className="card container">
-
+            <div className="card ">
+                <img src={`../public/photos/${props.img} `}/>
+                <span></span>
+                <span>{props.location}</span>
+                <span>{props.mapsURL}</span>
+                <p><strong>{props.start} - {props.end}</strong></p>
+                <p>{props.desc}</p>
 
 
             </div>
--- src/data.js
+++ src/data.js
@@ -7,7 +7,7 @@ export default [
         startDate: "11 Aug, 2021",
         endDate: "28 Aug, 2021",
         description: "Montego Bay, the capital of Saint James Parish on Jamaica’s north coast. Popular beaches include Doctor’s Cave Beach and Walter Fletcher Beach",
-        imageUrl: "../public/photos/jamaica-pic.jpg"
+        imageUrl: "jamaica-pic.jpg"
     },
     {
         id:2,
@@ -17,7 +17,7 @@ export default [
         startDate: "12 Apr, 2018",
         endDate: "26 Apr, 2018",
         description: "Orlando, a city in central Florida, is home to more than a dozen theme parks. Chief among its claims to fame is Walt Disney World.  A large, well-known theme park featuring movie-themed rides & shows, plus hotels & restaurants.",
-        imageUrl: "../public/photos/orlando.jpg"
+        imageUrl: "orlando.jpg"
     },
     {   
         id:3,
@@ -27,7 +27,7 @@ export default [
         startDate: "06 Feb, 2017",
         endDate: "20 Feb, 2017",
         description: "Maracas Bay is a bay with sandy beach on the island of Trinidad. This popular beach with a number of facilities features quiet waters due to the size of its bay.",
-        imageUrl: "../public/photos/trinidad.jpg"
+        imageUrl: "trinidad.jpg"
     }
 
 ]
