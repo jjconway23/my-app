c615127fccc92aa625c73810ce3cc876ad772ea7
Jacob <jacob.peat01@gmail.com>
2022-04-29 11:05:17 +0100

    add data to practise with

---
+++ src/data.js
@@ -0,0 +1,30 @@
+export default [
+    {
+        title: "The Caribbean Sea",
+        location: "Montego Bay, Jamaica",
+        googleMapsUrl: "https://goo.gl/maps/PZL81b5ukZ1i77G17",
+        startDate: "11 Aug, 2021",
+        endDate: "28 Aug, 2021",
+        description: "Montego Bay, the capital of Saint James Parish on Jamaica’s north coast. Popular beaches include Doctor’s Cave Beach and Walter Fletcher Beach",
+        imageUrl: "../public/photos/jamaica-pic.jpg"
+    },
+    {
+        title: "Universal Studios",
+        location: "Orlando, Florida",
+        googleMapsUrl: "https://goo.gl/maps/sWcL8BLvZf5LSm7Q6",
+        startDate: "12 Apr, 2018",
+        endDate: "26 Apr, 2018",
+        description: "Orlando, a city in central Florida, is home to more than a dozen theme parks. Chief among its claims to fame is Walt Disney World.  A large, well-known theme park featuring movie-themed rides & shows, plus hotels & restaurants.",
+        imageUrl: "../public/photos/orlando.jpg"
+    },
+    {
+        title: "Maracas Beach",
+        location: "Trinidad and Tobago",
+        googleMapsUrl: "https://goo.gl/maps/DGcBqJxSetAryD8K9",
+        startDate: "06 Feb, 2017",
+        endDate: "20 Feb, 2017",
+        description: "Maracas Bay is a bay with sandy beach on the island of Trinidad. This popular beach with a number of facilities features quiet waters due to the size of its bay.",
+        imageUrl: "../public/photos/trinidad.jpg"
+    }
+
+]
