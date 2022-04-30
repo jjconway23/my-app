679a02d33854618bb6f94fd853aca42a3127e2c3
Jacob <jacob.peat01@gmail.com>
2022-04-30 17:13:30 +0100

    Add styles to location container

--- src/Main.js
+++ src/Main.js
@@ -8,10 +8,11 @@ export default function Main(props){
                 
                 <img src={`./photos/${props.imageUrl} `} className="card-img"/>
                 <div className="card-data flex">
-                    <p>
-                    <span></span>
-                    <span>{props.location}</span>
-                    <span>{props.mapsURL}</span>
+                    <p className="location-container">
+                    <span className="location-pin">📍</span>
+                    <span className="location-name">{props.location}</span>
+                    <span ></span>
+                    <a className="google-maps-link" href={props.mapsURL} target="_blank">View in google maps!</a>
                     </p>                
                     <h2>{props.title}</h2>
                     <p><strong>{props.start} - {props.end}</strong></p>
